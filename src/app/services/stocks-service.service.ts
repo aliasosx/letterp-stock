e2105ec02fe752e8404ce4fe305ceb88d2130e70
alisaosx <aliasosx@hotmail.com>
2019-01-31 16:19:13 +0700

    udpate stock service

--- src/app/services/stocks-service.service.ts
+++ src/app/services/stocks-service.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@angular/core';
-import { FirebaseFirestore } from 'angularfire2';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StocksServiceService {
 
-  constructor(private db: FirebaseFirestore) { }
-  
-  updateStock() {
+  constructor() { }
 
-  }
 }
